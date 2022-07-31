1412851a40f4a0eda5248bc2d810429335042dd9
watkinsr <ryanwatkins54@gmail.com>
2022-07-31 20:08:44 +0100

    basic CMake (not working)

---
+++ Meta/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+ARG0=$0
+print_help() {
+    NAME=$(basename "$ARG0")
+    cat <<EOF
+Usage: $NAME COMMAND
+    $NAME run
+        Runs the thing
+EOF
+}
+
+die() {
+    >&2 echo "die: $*"
+    exit 1
+}
+
+usage() {
+    >&2 print_help
+    exit 1
+}
+
+CMD=$1
+case "$CMD" in
+    build)
+        mkdir -p build && cd build && cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -S ../ -B . && ninja;
+        ;;
+    run)
+        ./target/rpg;
+        ;;
+    *)
+        >&2 echo "Unknown command: $CMD"
+        usage
+        ;;
+esac
+
