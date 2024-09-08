fc767bc25d2ad6c236ac49439769eff9f286f100
Ryan <watkiryn@ryanwatkins.me>
2024-09-08 14:50:12 +0100

    NOJIRA: Temporary commit buffer.

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,17 +2,17 @@
 //#include <vld.h>
 #include "GameLoop.h"
 
-const string g_FONT_FILE = "res/fonts/Minecraftia.ttf";
-
-using namespace std;
-
-int main()
-{
-	GameLoop*  mainGame = new GameLoop(640, 480);
-	// mainGame->Begin(false);
-
-	delete mainGame;
-
-	return 0;
-}
+// const string g_FONT_FILE = "res/fonts/Minecraftia.ttf";
+// 
+// using namespace std;
+// 
+// int main()
+// {
+// 	GameLoop*  mainGame = new GameLoop(640, 480);
+// 	// mainGame->Begin(false);
+// 
+// 	delete mainGame;
+// 
+// 	return 0;
+// }
 
