8ad415d3088944796dcade6c4bb42c8191f16c39
Ryan Watkins <ryanwatkins54@gmail.com>
2024-09-09 02:41:00 +0100

    NOJIRA: Draw the play button again.

--- include/Game.h
+++ include/Game.h
@@ -5,6 +5,7 @@
 #include <SDL_render.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <vector>
 #include <string>
 
@@ -14,11 +15,15 @@ public:
     ~Game();
     SDL_Event* GetEvent();
     void LoadTexture(std::string);
+    void AllocateTextTexture(SDL_Color, std::string);
+    std::pair<int, int> GetTextureDimensions(SDL_Texture*);
     void Draw();
 private:
     SDL_Window*               _window;
     SDL_Renderer*             _renderer;
     SDL_Event*                _event;
+    TTF_Font*                 _font;
+    SDL_Surface*              _main_menu_play_surface;
     std::vector<SDL_Texture*> _textures = std::vector<SDL_Texture*>();
     const uint32_t SCREEN_WIDTH = 640;
     const uint32_t SCREEN_HEIGHT = 480;
