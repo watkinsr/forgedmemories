1ab629f7f84564bf5aaeba05b349e417942477cb
Ryan <watkiryn@ryanwatkins.me>
2024-09-09 04:22:05 +0100

    NOJIRA: Rudimentary scene stack.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include "Game.h"
 #include <memory>
 
+void handleSpaceKey(std::unique_ptr<Game>& game) {
+    game->IncrementSceneStack();
+}
+
 int main() {
     std::unique_ptr<Game> game = std::make_unique<Game>();
-    game->LoadTexture("assets/menu.gif");
-    game->AllocateTextTexture({255,255,255,255}, "Play");
+    game->InitDefaultScene();
     SDL_Event* e = game->GetEvent();
     while (e->type != SDL_QUIT){
         switch(e->type) {
             case SDL_KEYDOWN:
                 if (e->key.keysym.sym == SDLK_SPACE) {
                     printf("Detected keydown on <SPC>\n");
+                    handleSpaceKey(game);
                 }
                 break;
             default:
