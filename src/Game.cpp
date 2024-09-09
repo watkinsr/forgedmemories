4678cb98d61636902333940a518a5957daa7064f
Ryan Watkins <watkiryn@ryanwatkins.me>
2024-09-09 02:41:00 +0100

    NOJIRA: Draw the play button again.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,14 +1,65 @@
 #include "Game.h"
 #include <string>
 
+void allocate_play_texture();
+
+static constexpr std::string getDefaultFontPath() {
+    // FIXME: Implement a way to get a default monospaced font.
+    return "/usr/share/fonts/truetype/freefont/FreeMono.ttf";
+}
+
 Game::Game() {
-    SDL_Init(SDL_INIT_VIDEO);
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Panic: SDL initialization failed, abort.\n");
+    }
+
+    if (TTF_Init() < 0) {
+        printf("Panic: Failed to initialize SDL_ttf, abort.\n");
+    }
+
     _window = SDL_CreateWindow("Game", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (_window == NULL) {
+    if (!_window) {
         printf("panic: Window creation failed, abort.\n");
     }
-    _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
+    if (!_renderer) {
+        printf("panic: SDL Renderer creation failed, abort.\n");
+    }
+
+    _font = TTF_OpenFont(getDefaultFontPath().c_str(), 24);
+    if (!_font) {
+        printf("Panic: Failed to load font, abort.\n");
+    }
+
     _event = new SDL_Event();
+}
+
+std::pair<int, int> Game::GetTextureDimensions(SDL_Texture* texture) {
+    int width = 0;
+    int height = 0;
+    if (SDL_QueryTexture(texture, NULL, NULL, &width, &height) != 0) {
+        printf("Panic: Failed to query texture, abort.\n");
+    }
+
+    return std::pair(width, height);
+}
+
+void Game::AllocateTextTexture(SDL_Color color, std::string text) {
+    SDL_Surface* surface = TTF_RenderText_Solid(
+        _font,
+        text.c_str(),
+        color
+    );
+    if (!surface) {
+        printf("Panic: Failed to obtain surface, abort.\n");
+    }
+
+    SDL_Texture* text_texture = SDL_CreateTextureFromSurface(_renderer, surface);
+    if (!text_texture) {
+        printf("Panic: Failed to create texture for text, abort.\n");
+    }
+    _textures.push_back(text_texture);
 }
 
 Game::~Game() {
@@ -21,18 +72,33 @@ SDL_Event* Game::GetEvent() {
 }
 
 void Game::Draw() {
+    // FIXME: Texture rectangles should be abstracted.
+    // FIXME: All textures and their rectangles should be drawn.
+
+    SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255);
+    SDL_RenderClear(_renderer);
+
     SDL_Rect texture_rect;
     texture_rect.x = 0;
     texture_rect.y = 0;
     texture_rect.w = SCREEN_WIDTH;
     texture_rect.h = SCREEN_WIDTH;
+    SDL_RenderCopy(_renderer, _textures[0], NULL, &texture_rect);
 
-    // FIXME: Texture rectangles should be abstracted.
-    // FIXME: All textures and their rectangles should be drawn.
+    // FIXME: Draw Play button as text inside a rect.
 
-    SDL_RenderClear(_renderer);
-    SDL_RenderCopy(_renderer, _textures[0], NULL, &texture_rect); 
-    SDL_RenderPresent(_renderer); //updates the renderer    
+    std::pair<int, int> play_button_dims = GetTextureDimensions(_textures[1]);
+    SDL_Rect play_button_rect;
+    int play_width = std::get<0>(play_button_dims);
+    int play_height = std::get<1>(play_button_dims);
+    play_button_rect.x = SCREEN_WIDTH/2 - (play_width/2);
+    play_button_rect.y = SCREEN_HEIGHT/2 - 1;
+    play_button_rect.w = play_width;
+    play_button_rect.h = play_height;
+
+    SDL_RenderCopy(_renderer, _textures[1], NULL, &play_button_rect);
+
+    SDL_RenderPresent(_renderer);
 }
 
 void Game::LoadTexture(std::string path) {
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::unique_ptr<Game> game = std::make_unique<Game>();
     game->LoadTexture("assets/menu.gif");
+    game->AllocateTextTexture({255,255,255,255}, "Play");
     while (game->GetEvent()->type != SDL_QUIT){
         game->Draw();
 	}
