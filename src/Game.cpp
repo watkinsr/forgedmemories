bafe80e433fc2f46037cc957213781128760dd26
Ryan Watkins <ryanwatkins54@gmail.com>
2024-09-09 03:11:00 +0100

    NOJIRA: Bail out on panic.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,35 +1,42 @@
 #include "Game.h"
 #include <string>
+#include <cstdlib>
 
 void allocate_play_texture();
 
 static constexpr std::string getDefaultFontPath() {
     // FIXME: Implement a way to get a default monospaced font.
-    return "/usr/share/fonts/truetype/freefont/FreeMono.ttf";
+    // return "/usr/share/fonts/truetype/freefont/FreeMono.ttf";
+    return "/usr/share/fonts/liberation-mono-fonts/LiberationMono-Regular.ttf";
 }
 
 Game::Game() {
     if(SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Panic: SDL initialization failed, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     if (TTF_Init() < 0) {
         printf("Panic: Failed to initialize SDL_ttf, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     _window = SDL_CreateWindow("Game", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!_window) {
         printf("panic: Window creation failed, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
     if (!_renderer) {
         printf("panic: SDL Renderer creation failed, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     _font = TTF_OpenFont(getDefaultFontPath().c_str(), 24);
     if (!_font) {
         printf("Panic: Failed to load font, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     _event = new SDL_Event();
@@ -40,6 +47,7 @@ std::pair<int, int> Game::GetTextureDimensions(SDL_Texture* texture) {
     int height = 0;
     if (SDL_QueryTexture(texture, NULL, NULL, &width, &height) != 0) {
         printf("Panic: Failed to query texture, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     return std::pair(width, height);
@@ -53,11 +61,13 @@ void Game::AllocateTextTexture(SDL_Color color, std::string text) {
     );
     if (!surface) {
         printf("Panic: Failed to obtain surface, abort.\n");
+        exit(EXIT_FAILURE);
     }
 
     SDL_Texture* text_texture = SDL_CreateTextureFromSurface(_renderer, surface);
     if (!text_texture) {
         printf("Panic: Failed to create texture for text, abort.\n");
+        exit(EXIT_FAILURE);
     }
     _textures.push_back(text_texture);
 }
@@ -103,6 +113,7 @@ void Game::LoadTexture(std::string path) {
     SDL_Texture* texture = IMG_LoadTexture(_renderer, path.c_str());
     if (texture == NULL){
         printf("Panic: Failed to load texture at %s.\n", path.c_str());
+        exit(EXIT_FAILURE);
 	} else {
         _textures.push_back(texture);
     }
