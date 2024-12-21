e65d703841476b823d542483fe98b9fc788d1c89
Ryan Watkins <ryanwatkins54@gmail.com>
2024-12-21 00:36:05 +0000

    Misc: Cleanup project

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -x
-
-g++ -Wall -Wextra -pedantic -O2 src/Common.cpp src/MapEditor.cpp -lSDL2  -lSDL2_mixer  -lSDL2  -lSDL2_ttf  -lSDL2  -lSDL2_image  -lSDL2  -lSDL2_mixer  -lSDL2_ttf  -lSDL2_image -g -I./include/ -I/usr/include/SDL2 -std=c++20 -o MapEditor
